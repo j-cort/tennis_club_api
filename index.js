5ca8effe72a18db8a98717c01a1ea22f9e92b77a
j-cort <81960011+j-cort@users.noreply.github.com>
2022-03-12 14:52:27 +0000

    filter get players query by rank and nationality

--- index.js
+++ index.js
@@ -16,21 +16,73 @@ const pool = new pg.Pool({
 })
 
 app.get("/players", async (req, res) => {
-  const { rows } = await pool.query(`
-    SELECT 
-    id, 
-    name, 
-    RANK () OVER (ORDER BY points DESC) AS position,
-    rank_name,
-    points, 
-    nationality, 
-    age
-    FROM joint_player_details;
+  const { rank_name, nationality } = req.body
+
+  if(rank_name && nationality) {
+    const { rows } = await pool.query(`
+      SELECT 
+      id, 
+      name, 
+      RANK () OVER (ORDER BY points DESC) AS position,
+      rank_name,
+      points, 
+      nationality, 
+      age
+      FROM joint_player_details
+      WHERE rank_name = $1 AND nationality = $2;`,
+      [rank_name, nationality]
+      )
+      res.send(rows) 
+  } else if (rank_name) {
+    const { rows } = await pool.query(`
+      SELECT 
+      id, 
+      name, 
+      RANK () OVER (ORDER BY points DESC) AS position,
+      rank_name,
+      points, 
+      nationality, 
+      age
+      FROM joint_player_details
+      WHERE rank_name = $1;`,
+      [rank_name]
+    )
+    res.send(rows) 
+  } else if (nationality) {
+    const { rows } = await pool.query(`
+      SELECT 
+      id, 
+      name, 
+      RANK () OVER (ORDER BY points DESC) AS position,
+      rank_name,
+      points, 
+      nationality, 
+      age
+      FROM joint_player_details
+      WHERE nationality = $1;`,
+      [nationality]
+    )
+    res.send(rows) 
+  } else {
+    const { rows } = await pool.query(`
+      SELECT 
+      id, 
+      name, 
+      RANK () OVER (ORDER BY points DESC) AS position,
+      rank_name,
+      points, 
+      nationality, 
+      age
+      FROM joint_player_details;
   `)
    res.send(rows) 
+  }
+
+ 
 });
 
 app.post("/players", async (req, res) => {
+
   const { first_name, last_name, nationality, date_of_birth } = req.body
   const { rows } = await pool.query(`
   INSERT INTO players (first_name, last_name, nationality, date_of_birth)
@@ -40,7 +92,7 @@ app.post("/players", async (req, res) => {
 });
 
 app.post("/matches", async (req, res) => {
-  console.log(req.body)
+  // business logic
   const { winner_id, loser_id } = req.body
   const { rows } = await pool.query(`
   INSERT INTO matches (winner_id, loser_id)
